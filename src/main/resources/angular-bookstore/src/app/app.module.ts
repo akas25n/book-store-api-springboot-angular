05907365f37677ec8449d7812d14d8648733cb96
akas25n <akas25n@gmail.com>
2020-11-26 22:09:47 +0100

    completed feature three, search by category

--- src/main/resources/angular-bookstore/src/app/app.module.ts
+++ src/main/resources/angular-bookstore/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BookService} from "./services/book.service";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
